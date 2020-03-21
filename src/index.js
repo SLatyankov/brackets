dac9095bd622858e9d4f489aa5aa2635f8647542
SLatyankov <Slatiankov@yandex.ru>
2020-03-22 00:14:45 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var obj = {};
+    bracketsConfig.forEach(element => {
+        obj[element[0]] = element[1];
+    });
+    var arr = str.split('');
+    console.log(arr);
+
+    for (j = 0; j < 10; j++) {
+        for (i = 0; i < arr.length - 1; i++) {
+
+            if (obj[arr[i]] === arr[i + 1]) {
+                arr.splice(i, 2);
+                i = 0;
+            };
+        }
+
+    }
+    console.log(arr);
+    return arr.length == 0;
 }
